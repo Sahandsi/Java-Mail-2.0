d5b0db5d5f38eb1029a374cb349cff812dcf94ad
Sam <sahand@pc-141-190.customer.sovereign.fortis.lan>
2018-03-03 19:59:58 +0000

    Sql done server now

--- src/Client.java
+++ src/Client.java
@@ -4,60 +4,74 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.*;
-//import java.io.PrintWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.IOException;
-import java.sql.*;
-
 
 public class Client extends Application // for GUI
-
 {
-private PrintWriter outputToServer ; // send message to server
-private Scanner inputFromServer;    // gets response back from the server
-private Socket socket;
+
+    private PrintWriter outputToServer; // send message to server
+    private Scanner inputFromServer;    // gets response back from the server
+    //private String username;
+    private Socket socket;
     private static InetAddress host = null;
-//    final int PORT = 1234;
-    Connection link = null;
-    Statement statement = null;
-    ResultSet results = null;
+    final int PORT = 1234;
 
-
-
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args) {
         try {
             host = InetAddress.getLocalHost();
-            }
-
-            catch (UnknownHostException ex)
-            {
+        } catch (UnknownHostException ex) {
             System.out.println("Host ID Not Found");
-            }
+        }
 
-        do
-            {
+        do {
             launch(args);
-            }
-
-            while (true);
+        } while (true);
 
     }
 
+    public void loader() throws Exception
+    {
+        socket = new Socket(host, PORT);
+        // scanner set up so that it can scan for any input stream (responses) that come from the server
+        inputFromServer = new Scanner(socket.getInputStream());
+        outputToServer = new PrintWriter(socket.getOutputStream(), true);
+
+    }
+
+//    public void start(Stage stage) throws Exception {
+//        // set up variables
+//
+////        socket = new Socket(host, PORT);
+////        // scanner set up so that it can scan for any input stream (responses) that come from the server
+////        inputFromServer = new Scanner(socket.getInputStream());
+////        outputToServer = new PrintWriter(socket.getOutputStream(), true);
+//
+//
+//        Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+//        Scene scene;
+//        scene = new Scene(root, 500, 500);
+//        //add the scene to the stage
+//        stage = new Stage();
+//        stage.setScene(scene);
+//        stage.show();
+//
+//    }
 
     @FXML
     public void start(Stage stage) throws Exception
     {
+
         // set up variables
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
         Scene scene;
         scene = new Scene(root, 640, 450);
         //add the scene to the stage
@@ -67,6 +81,82 @@ private Socket socket;
 
     }
 
+
+
+    @FXML
+    private TextField Input;
+
+    @FXML
+    private void loginButton (ActionEvent event) throws Exception
+    {
+
+        String t = Input.getText();
+        //validateUsername(t);
+        System.out.println(t);
+        validateUsername(t);
+    }
+
+
+    @FXML
+    private void validateUsername(String t) throws Exception {
+//        if (username.isEmpty()) {
+//           // message.setText("Please enter your username");
+//        } else {
+//           //  send username across to the server
+//        System.out.println(username);
+//String u;
+//u= username;
+
+            loader();
+            outputToServer.println(t);
+            String serverRequest = inputFromServer.nextLine();
+
+            if (serverRequest.equals("true")) {
+                LoadClient();
+            }
+        //}
+    }
+
+    private void LoadClient() throws Exception
+    {
+//        Scene scene;
+//        VBox vbox;
+//        Stage stage;
+//
+//        Button inbox = new Button("Inbox");
+//        Button email = new Button("Email");
+//        Button quit = new Button("Quit");
+//
+////        inbox.setOnAction(e -> getInbox());
+////        //email.setOnAction(e -> getEmail());
+////        quit.setOnAction(e -> quitApp());
+////
+////        // add buttons to layout
+//        vbox = new VBox();
+////        vbox.getChildren().add(inbox);
+////        vbox.getChildren().add(email);
+////        vbox.getChildren().add(quit);
+//
+//        scene = new Scene(vbox, 500, 500);
+//        stage = new Stage();
+//        stage.setScene(scene);
+//        stage.show();
+        Scene scene;
+        Stage stage;
+       // closeButtonAction();
+
+        Parent root = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
+        scene = new Scene(root, 800, 500);
+        stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void getInbox() {
+        System.out.println("BEFORE SENDING INBOX REQUEST");
+        outputToServer.println("get_inbox");
+        System.out.println("AFTER SENDING INBOX REQUEST");
+    }
 
     @FXML
     private void composeButtonHandler(ActionEvent event) throws Exception
@@ -82,183 +172,31 @@ private Socket socket;
         stage.show();
     }
 
-
-    @FXML private Label message;
-
-    @FXML
-    private TextField Input;
-
-    @FXML
-    private void sendMail()
-    {
+    public void getEmail() throws Exception {
+        //outputToServer.println("get_email"); // send request
+        ObjectInputStream recieveObjFromServer = null; // recieve obj from serber
 
 
-
-
-    }
-
-    private void getEmail()
-    {
-
-
-    }
-
-//
-//
-//    private void connection(){
-//        try {
-//            Class.forName("com.mysql.jdbc.Driver");
-//            link = DriverManager.getConnection("jdbc:mysql://localhost:8889/Java", "root", "root");
-//        }
-//        catch(Exception ex)
-//        {
-//
-//        }
-//    }
-
-    private static Connection getConnection()
-    {
-        try{
-            String driver = "com.mysql.jdbc.Driver";
-            String url = "jdbc:mysql://localhost:8889/Java";
-            String username = "root";
-            String password = "root";
-
-            Class.forName(driver);
-
-            Connection conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected");
-            return conn;
-
-        } catch(Exception e){
-            System.out.println(e);
-        }
-        return null;
-    }
-
-    @FXML
-    private TextField registerInput;
-
-    @FXML
-    private void registerUser() throws Exception{
-        String t = registerInput.getText();
-
-        try{
-            Connection con = getConnection();
-            PreparedStatement posted = con.prepareStatement("INSERT INTO Users (Name) VALUES ('"+t+"')");
-
-            posted.executeUpdate();
-        }catch (Exception e){
-            System.out.println(e);
-        }
-
-        finally {
-            System.out.println("Insert complete");
-        }
-
-    }
-
-
-    @FXML
-    private void validateUsername()
-    {
-        String t = Input.getText();
-
-    try {
-        Connection con = getConnection();
-        //connection();
-        statement = con.createStatement();
-        String sql = "SELECT Name from Users WHERE Name LIKE \'"+ t + "\'";
-
-        System.out.println(sql);
-        results = statement.executeQuery(sql);
-        int count =0;
-        while(results.next())
+        try
         {
-            count = count+1;
+             recieveObjFromServer = new ObjectInputStream(socket.getInputStream());
         }
-
-        System.out.println(count);
-        if (count ==1){
-        System.out.println("user found");
-        Scene scene;
-        Stage stage;
-        closeButtonAction();
-
-        Parent root = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
-        scene = new Scene(root, 800, 500);
-        stage = new Stage();
-        stage.setScene(scene);
-        stage.show();
-
-    }
-
-        else if (count>1)
+        catch (IOException e)
         {
-            System.out.println("two users!!!!");
-        }
-            else{
-                System.out.println("no user!");
-            }
-
-            }
-            catch(Exception ex)
-        {
-
+            e.printStackTrace();
         }
 
-
-    }
-
-    @FXML private javafx.scene.control.Button login;
-
-    //For closing the old Scene once user logs in.
-    @FXML
-    private void closeButtonAction(){
-        // get a name of the button to the stage
-        Stage stage = (Stage) login.getScene().getWindow();
-        // close the old stage
-        stage.close();
-    }
-//
-//        public void getEmail() throws Exception {
-//        //outputToServer.println("get_email"); // send request
-//        ObjectInputStream recieveObjFromServer = null; // recieve obj from serber
-//
-//        try
-//        {
-//            recieveObjFromServer = new ObjectInputStream(socket.getInputStream());
-//        }
-//        catch (IOException e)
-//        {
-//            e.printStackTrace();
-//        }
-//
-//        ArrayList<Email> inbox;
-//
-//
-//        inbox = (ArrayList<Email>) recieveObjFromServer.readObject();
-//
-//        // now display this in a table view or
-//
-//    }
+        ArrayList<Email> inbox;
 
 
-    public void getInbox() {
-        System.out.println("BEFORE SENDING INBOX REQUEST");
-//        outputToServer.println("get_inbox");
-        System.out.println("AFTER SENDING INBOX REQUEST");
+        inbox = (ArrayList<Email>) recieveObjFromServer.readObject();
+
+        // now display this in a table view or
+
     }
 
     public void quitApp() {
-       // outputToServer.println("close");
-        System.exit(0);
+        outputToServer.println("close");
     }
+
 }
-
-
-
-
-
-
-
--- src/Server.java
+++ src/Server.java
@@ -1,28 +1,77 @@
-
 import javafx.fxml.FXML;
 import java.io.IOException;
 import java.io.Serializable;
 import java.lang.reflect.Array;
 import java.net.ServerSocket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
 
-public class Server implements Serializable // used to send object from     client to server
+public class Server implements Serializable // used to send object from client to server
 {
     // list of users of type string
     private static ArrayList<String> users = new ArrayList<String>();
     private static ArrayList<Email> mails = new ArrayList<Email>();
 
+
+    private static Connection getConnection()
+    {
+        try{
+            Connection link = null;
+            Statement statement = null;
+            ResultSet results = null;
+            String driver = "com.mysql.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:8889/Java";
+            String username = "root";
+            String password = "root";
+
+            Class.forName(driver);
+
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected");
+          //  Connection conn = getConnection();
+            //connection();
+            statement = conn.createStatement();
+            String sql = "SELECT Name from Users";
+
+
+            System.out.println(sql);
+            ResultSet rs = statement.executeQuery(sql);
+           // return conn;
+
+            while (rs.next()) {
+
+
+                String names = rs.getString("Name");
+                System.out.println("names : " + names);
+users.add(names);
+
+            }
+
+
+        } catch(Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
+
     public static void main(String[] args) throws IOException
     {
         // set up 3 users
-        users.add("U1");
-        users.add("U2");
-        users.add("U3");
+//        users.add("U1");
+//        users.add("U2");
+//        users.add("U3");
+
+          getConnection();
+
 
         Socket client; // client
+
         ServerSocket serverSocket = null; // server
         final int PORT = 1234;
 
@@ -42,8 +91,7 @@ public class Server implements Serializable // used to send object from     clie
         System.out.println("\n Server running");
 
         do {
-            client = serverSocket.accept(); // accept the client to the   server
-            System.out.println("Client accepted");
+            client = serverSocket.accept(); // accept the client to the server
             // create a function that will validate the user
             String validUser = validateUser(client);
             clientHandler = new ClientHandler(validUser, client);
@@ -54,18 +102,21 @@ public class Server implements Serializable // used to send object from     clie
 
     }
 
+
     private static String validateUser(Socket client)
     {
         Scanner inputFromClient = null;
         PrintWriter outputToClient = null;
         boolean validUser = false;
 
+
+
         try
         {
             // allows the server to retrieve the input from the client
             inputFromClient = new Scanner(client.getInputStream());
             // allow server to send things to the client
-            outputToClient = new PrintWriter(client.getOutputStream(),  true);
+            outputToClient = new PrintWriter(client.getOutputStream(), true);
 
         }
         catch(IOException io)
@@ -73,16 +124,18 @@ public class Server implements Serializable // used to send object from     clie
             System.out.println("Problem initialising variables");
         }
 
+
         // get the input from the client
-        System.out.printf("BEFORE THE VALUE");
         String userToValidate = inputFromClient.nextLine();
+
 
 
         while  (validUser == false)
         {
             for(String username : users)
             {
-                // check the user to validate matches the user from the  client
+
+                // check the user to validate matches the user from the client
                 if (username.equals(userToValidate))
                 {
                     // tell the client that user is valid
@@ -95,7 +148,7 @@ public class Server implements Serializable // used to send object from     clie
             {
                 // user is invalid so wait for a new user to pass from the client to the server
                 outputToClient.println("false");
-                userToValidate = inputFromClient.nextLine();
+                userToValidate = inputFromClient.next();
             }
             else
             {
@@ -115,39 +168,54 @@ public class Server implements Serializable // used to send object from     clie
         return mails;
     }
 
+
 }
+
 // each client will have their unique username
-class ClientHandler extends Thread implements Serializable {
+class ClientHandler extends Thread implements Serializable
+{
     private Socket client;
     // retrieve requests from the client
     private Scanner input;
     // send requests to the client
     private PrintWriter output;
+
     private String username;
 
-    public ClientHandler(String username, Socket client) {
+    public ClientHandler(String username, Socket client)
+    {
         this.username = username;
         this.client = client;
-        try {
+        System.out.println("BEFORE TRY");
+        try
+        {
             input = new Scanner(client.getInputStream());
             output = new PrintWriter(client.getOutputStream(), true);
-        } catch (IOException io) {
+        }
+
+        catch(IOException io)
+        {
             System.out.println("Client Handler not set up properly");
         }
 
     }
 
-    public void run() {
+    public void run()
+    {
         // recieve request from the server
         String request = input.nextLine();
 
         System.out.println(request);
         // check the request
-        while (!request.equals("close")) {
+        while(!request.equals("close"))
+        {
             // do whatever the user wants to do
-            if (request.equals("get_inbox")) {
+            if (request.equals("get_inbox"))
+            {
                 System.out.println("INSIDE INBOX REQUEST");
-            } else if (request.equals("send_email")) {
+            }
+            else if (request.equals("send_email"))
+            {
                 System.out.println("INSIDE SEND EMAIL REQUEST");
             }
 
@@ -155,13 +223,15 @@ class ClientHandler extends Thread implements Serializable {
         }
 
         // end the client connection
-        try {
+        try
+        {
             System.out.println("Ending connection");
             client.close();
-        } catch (IOException io) {
+        }
+        catch(IOException io)
+        {
             System.out.println("Coulnd't close connection");
         }
-
 
     }
 
